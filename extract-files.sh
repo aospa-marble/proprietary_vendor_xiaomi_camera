90af3069ce6b3c3c018a1bb5a8611ebaa16a3407
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-07-17 11:51:05 +0530

    marble-camera: Update to V816.0.6.0.UMRMIXM

--- extract-files.sh
+++ extract-files.sh
@@ -63,8 +63,11 @@ function blob_fixup() {
             ;;
         system/priv-app/MiuiCamera/MiuiCamera.apk)
             tmp_dir="${EXTRACT_TMP_DIR}/MiuiCamera"
+            echo "    - Extracting apk..."
             $APKTOOL d -q "$2" -o "$tmp_dir" -f
+            echo "    - Patching apk..."
             grep -rl "com.miui.gallery" "$tmp_dir" | xargs sed -i 's|"com.miui.gallery"|"com.google.android.apps.photos"|g'
+            echo "    - Rebuilding apk..."
             $APKTOOL b -q "$tmp_dir" -o "$2"
             rm -rf "$tmp_dir"
             split --bytes=20M -d "$2" "$2".part
